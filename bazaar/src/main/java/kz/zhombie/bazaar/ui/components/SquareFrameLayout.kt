c2a52d3b02dccb7d04b0ebdd849b040fc76f87f5
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2021-03-04 23:21:35 +0500

    Checkbox drawables

---
+++ bazaar/src/main/java/kz/zhombie/bazaar/ui/components/SquareFrameLayout.kt
@@ -0,0 +1,22 @@
+package kz.zhombie.bazaar.ui.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+internal class SquareFrameLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr, defStyleRes) {
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        if (widthMeasureSpec > heightMeasureSpec) {
+            super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+        } else {
+            super.onMeasure(heightMeasureSpec, heightMeasureSpec)
+        }
+    }
+
+}
