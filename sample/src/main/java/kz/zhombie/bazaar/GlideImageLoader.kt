12a392b57a889193f4d4a70097ebfb6776588840
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2021-03-06 01:05:57 +0500

    Added Glide as alternative image loader

---
+++ sample/src/main/java/kz/zhombie/bazaar/GlideImageLoader.kt
@@ -0,0 +1,38 @@
+package kz.zhombie.bazaar
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.net.Uri
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import kz.zhombie.bazaar.api.ImageLoader
+
+class GlideImageLoader : ImageLoader {
+
+    override fun loadGridItemImage(context: Context, imageView: ImageView, uri: Uri) {
+        Glide.with(context)
+            .load(uri)
+            .centerCrop()
+            .error(R.drawable.ic_placeholder_white)
+            .fallback(R.drawable.ic_placeholder_white)
+            .override(300, 300)
+            .placeholder(R.drawable.ic_placeholder_white)
+            .into(imageView)
+    }
+
+    override fun loadGridItemImage(context: Context, imageView: ImageView, bitmap: Bitmap) {
+        imageView.setImageBitmap(bitmap)
+    }
+
+    override fun loadFullscreenImage(context: Context, imageView: ImageView, uri: Uri) {
+        Glide.with(context)
+            .load(uri)
+            .centerCrop()
+            .error(R.drawable.ic_placeholder_black)
+            .fallback(R.drawable.ic_placeholder_black)
+            .override(1280, 960)
+            .placeholder(R.drawable.ic_placeholder_black)
+            .into(imageView)
+    }
+
+}
--- sample/src/main/java/kz/zhombie/bazaar/MainActivity.kt
+++ sample/src/main/java/kz/zhombie/bazaar/MainActivity.kt
@@ -5,31 +5,68 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
+import coil.Coil
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textview.MaterialTextView
+import kz.zhombie.bazaar.api.ImageLoader
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         private val TAG: String = MainActivity::class.java.simpleName
+
+        private val DEFAULT_IMAGE_LOAD = "Coil" to CoilImageLoader()
     }
 
     private object RequestCode {
         const val READ_EXTERNAL_STORAGE = 100
     }
 
-    private var button: MaterialButton? = null
+    private lateinit var imageLoaderView: MaterialTextView
+    private lateinit var imageLoaderButton: MaterialButton
+    private lateinit var showButton: MaterialButton
+
+    private lateinit var imageLoader: ImageLoader
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button = findViewById(R.id.button)
+        imageLoaderView = findViewById(R.id.imageLoaderView)
+        imageLoaderButton = findViewById(R.id.imageLoaderButton)
+        showButton = findViewById(R.id.showButton)
 
-        button?.setOnClickListener {
+        imageLoaderView.text = DEFAULT_IMAGE_LOAD.first
+        imageLoader = DEFAULT_IMAGE_LOAD.second
+
+        imageLoaderButton.setOnClickListener {
+            MaterialAlertDialogBuilder(this)
+                .setSingleChoiceItems(arrayOf("Coil", "Glide"), -1) { dialog, which ->
+                    dialog.dismiss()
+                    when (which) {
+                        0 -> {
+                            imageLoader = CoilImageLoader()
+                            imageLoaderView.text = "Coil"
+                        }
+                        1 -> {
+                            imageLoader = GlideImageLoader()
+                            imageLoaderView.text = "Glide"
+                        }
+                        else -> {
+                            imageLoader = DEFAULT_IMAGE_LOAD.second
+                            imageLoaderView.text = DEFAULT_IMAGE_LOAD.first
+                        }
+                    }
+                }
+                .show()
+        }
+
+        showButton.setOnClickListener {
             if (checkPermissions()) {
                 Bazaar.Builder(supportFragmentManager)
                     .setTag(Bazaar.TAG)
-                    .setImageLoader(CoilImageLoader())
+                    .setImageLoader(imageLoader)
                     .show()
             }
         }
