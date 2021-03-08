c70207b2d405c5d7446bf3e8ca28f19352cf72d0
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2021-03-08 15:14:48 +0500

    New HeaderViewTitleButton

--- bazaar/src/main/java/kz/zhombie/bazaar/ui/components/view/HeaderView.kt
+++ bazaar/src/main/java/kz/zhombie/bazaar/ui/components/view/HeaderView.kt
@@ -2,19 +2,12 @@ package kz.zhombie.bazaar.ui.components.view
 
 import android.content.Context
 import android.graphics.Color
-import android.graphics.Typeface
-import android.text.InputFilter
 import android.util.AttributeSet
-import android.util.TypedValue
-import android.view.Gravity
 import android.view.View
-import android.widget.LinearLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
-import androidx.core.view.ViewCompat
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.imageview.ShapeableImageView
-import com.google.android.material.textview.MaterialTextView
 import kz.zhombie.bazaar.R
 import kz.zhombie.bazaar.utils.dp2Px
 import kotlin.math.roundToInt
@@ -27,10 +20,7 @@ internal class HeaderView @JvmOverloads constructor(
 ) : ConstraintLayout(context, attrs, defStyleAttr, defStyleRes) {
 
     private val scrollIndicatorView: ShapeableImageView
-    private val titleButton: LinearLayout
-    private lateinit var titleTextView: MaterialTextView
-    private lateinit var subtitleTextView: MaterialTextView
-    private lateinit var iconView: ShapeableImageView
+    private val titleButton: HeaderViewTitleButton
     private val closeButton: MaterialButton
 
     init {
@@ -49,124 +39,48 @@ internal class HeaderView @JvmOverloads constructor(
         addView(divider)
     }
 
-    private object Id {
-        const val SCROLL_INDICATOR_VIEW = 1
-
-        const val TITLE_BUTTON = 2
-        const val TITLE_TEXT_VIEW = 3
-        const val SUBTITLE_TEXT_VIEW = 4
-        const val ICON_VIEW = 5
-
-        const val CLOSE_BUTTON = 6
-
-        const val DIVIDER = 7
+    private object ViewId {
+        val SCROLL_INDICATOR_VIEW = hashCode() + 1
+        val TITLE_BUTTON = hashCode() + 2
+        val CLOSE_BUTTON = hashCode() + 3
+        val DIVIDER = hashCode() + 4
     }
 
     private fun createScrollIndicatorView(): ShapeableImageView {
         val imageView = ShapeableImageView(context)
-//        imageView.id = ViewCompat.generateViewId()
-        imageView.id = Id.SCROLL_INDICATOR_VIEW
+        imageView.id = ViewId.SCROLL_INDICATOR_VIEW
         val layoutParams = LayoutParams(35F.dp2Px().roundToInt(), 3F.dp2Px().roundToInt())
         layoutParams.setMargins(0, 7.5F.dp2Px().roundToInt(), 0, 0)
         layoutParams.leftToLeft = ConstraintSet.PARENT_ID
         layoutParams.topToTop = ConstraintSet.PARENT_ID
         layoutParams.rightToRight = ConstraintSet.PARENT_ID
-        layoutParams.bottomToTop = Id.TITLE_BUTTON
+        layoutParams.bottomToTop = ViewId.TITLE_BUTTON
         imageView.layoutParams = layoutParams
         imageView.setImageResource(R.drawable.ic_scroll_indicator)
         return imageView
     }
 
-    private fun createTitleButton(): LinearLayout {
-        val titleButton = LinearLayout(context)
-        titleButton.id = Id.TITLE_BUTTON
+    private fun createTitleButton(): HeaderViewTitleButton {
+        val titleButton = HeaderViewTitleButton(context)
+        titleButton.id = ViewId.TITLE_BUTTON
         val titleButtonsLayoutParams = LayoutParams(0, LayoutParams.WRAP_CONTENT)
         titleButtonsLayoutParams.setMargins(10F.dp2Px().roundToInt(), 5F.dp2Px().roundToInt(), 10F.dp2Px().roundToInt(), 0)
         titleButtonsLayoutParams.leftToLeft = ConstraintSet.PARENT_ID
-        titleButtonsLayoutParams.topToTop = Id.SCROLL_INDICATOR_VIEW
-        titleButtonsLayoutParams.rightToLeft = Id.CLOSE_BUTTON
+        titleButtonsLayoutParams.topToTop = ViewId.SCROLL_INDICATOR_VIEW
+        titleButtonsLayoutParams.rightToLeft = ViewId.CLOSE_BUTTON
         titleButton.layoutParams = titleButtonsLayoutParams
-        titleButton.setBackgroundResource(R.drawable.ripple_rounded)
-        titleButton.isClickable = true
-        titleButton.isFocusable = true
-        titleButton.gravity = Gravity.CENTER_VERTICAL
-        titleButton.orientation = LinearLayout.HORIZONTAL
-        titleButton.setPadding(15F.dp2Px().roundToInt(), 10F.dp2Px().roundToInt(), 15F.dp2Px().roundToInt(), 10F.dp2Px().roundToInt())
-
-        val titleViewContainer = LinearLayout(context)
-        titleViewContainer.id = ViewCompat.generateViewId()
-        val titleViewContainerLayoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-        titleViewContainer.layoutParams = titleViewContainerLayoutParams
-        titleViewContainer.orientation = LinearLayout.VERTICAL
-
-        titleTextView = createTitleTextView()
-        subtitleTextView = createSubtitleTextView()
-
-        titleViewContainer.addView(titleTextView)
-        titleViewContainer.addView(subtitleTextView)
-
-        iconView = createIconView()
-
-        titleButton.addView(titleViewContainer)
-        titleButton.addView(iconView)
-
         return titleButton
-    }
-
-    private fun createTitleTextView(): MaterialTextView {
-        val textView = MaterialTextView(context)
-        textView.id = Id.TITLE_TEXT_VIEW
-        val layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-        textView.layoutParams = layoutParams
-        textView.letterSpacing = 0F
-        textView.maxLines = 1
-        textView.filters = arrayOf(InputFilter.LengthFilter(18))
-        textView.typeface = Typeface.create(Typeface.SANS_SERIF, Typeface.BOLD)
-        textView.setSingleLine()
-        textView.isAllCaps = false
-        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 17F)
-        textView.setTextColor(Color.parseColor("#333333"))
-        return textView
-    }
-
-    private fun createSubtitleTextView(): MaterialTextView {
-        val textView = MaterialTextView(context)
-        textView.id = Id.SUBTITLE_TEXT_VIEW
-        val viewLayoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-        viewLayoutParams.setMargins(0, 1F.dp2Px().roundToInt(), 0, 0)
-        textView.layoutParams = viewLayoutParams
-        textView.letterSpacing = 0F
-        textView.maxLines = 1
-        textView.filters = arrayOf(InputFilter.LengthFilter(18))
-        textView.typeface = Typeface.create(Typeface.SANS_SERIF, Typeface.NORMAL)
-        textView.setSingleLine()
-        textView.isAllCaps = false
-        textView.includeFontPadding = false
-        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 11F)
-        textView.text = "Текущий альбом"
-        textView.setTextColor(Color.parseColor("#8290A0"))
-        return textView
-    }
-
-    private fun createIconView(): ShapeableImageView {
-        val imageView = ShapeableImageView(context)
-        imageView.id = Id.ICON_VIEW
-        val layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-        layoutParams.setMargins(15F.dp2Px().roundToInt(), 0, 15F.dp2Px().roundToInt(), 0)
-        imageView.layoutParams = layoutParams
-        imageView.setImageResource(R.drawable.ic_dropdown_down)
-        return imageView
     }
 
     private fun createCloseButton(): MaterialButton {
         val closeButton = MaterialButton(context, null, R.style.Widget_MaterialComponents_Button_TextButton_Icon)
-        closeButton.id = Id.CLOSE_BUTTON
+        closeButton.id = ViewId.CLOSE_BUTTON
         val closeButtonLayoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
         closeButtonLayoutParams.setMargins(5F.dp2Px().roundToInt(), 0, 5F.dp2Px().roundToInt(), 0)
-        closeButtonLayoutParams.leftToRight = Id.TITLE_BUTTON
-        closeButtonLayoutParams.topToTop = Id.TITLE_BUTTON
+        closeButtonLayoutParams.leftToRight = ViewId.TITLE_BUTTON
+        closeButtonLayoutParams.topToTop = ViewId.TITLE_BUTTON
         closeButtonLayoutParams.rightToRight = ConstraintSet.PARENT_ID
-        closeButtonLayoutParams.bottomToBottom = Id.TITLE_BUTTON
+        closeButtonLayoutParams.bottomToBottom = ViewId.TITLE_BUTTON
         closeButton.layoutParams = closeButtonLayoutParams
         closeButton.background = null
         closeButton.insetTop = 0
@@ -184,11 +98,11 @@ internal class HeaderView @JvmOverloads constructor(
 
     private fun createDivider(): View {
         val view = View(context)
-        view.id = Id.DIVIDER
+        view.id = ViewId.DIVIDER
         val layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, 0.75F.dp2Px().roundToInt())
         layoutParams.setMargins(0, 5F.dp2Px().roundToInt(), 0, 0)
         layoutParams.leftToLeft = ConstraintSet.PARENT_ID
-        layoutParams.topToBottom = Id.TITLE_BUTTON
+        layoutParams.topToBottom = ViewId.TITLE_BUTTON
         layoutParams.rightToRight = ConstraintSet.PARENT_ID
         view.layoutParams = layoutParams
         view.setBackgroundColor(Color.parseColor("#B6BDC6"))
@@ -196,15 +110,11 @@ internal class HeaderView @JvmOverloads constructor(
     }
 
     fun setTitle(title: String) {
-        titleTextView.text = title
+        titleButton.setTitle(title)
     }
 
     fun toggleIcon(isUp: Boolean) {
-        if (isUp) {
-            iconView.setImageResource(R.drawable.ic_dropdown_up)
-        } else {
-            iconView.setImageResource(R.drawable.ic_dropdown_down)
-        }
+        titleButton.toggleIcon(isUp)
     }
 
     fun setOnTitleButtonClickListener(callback: () -> Unit) {
---
+++ bazaar/src/main/java/kz/zhombie/bazaar/ui/components/view/HeaderViewTitleButton.kt
@@ -0,0 +1,94 @@
+package kz.zhombie.bazaar.ui.components.view
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Typeface
+import android.text.TextUtils
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.widget.LinearLayout
+import com.google.android.material.textview.MaterialTextView
+import kz.zhombie.bazaar.R
+import kz.zhombie.bazaar.utils.dp2Px
+import kotlin.math.roundToInt
+
+internal class HeaderViewTitleButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr, defStyleRes) {
+
+    private val titleTextView: MaterialTextView
+    private val subtitleTextView: MaterialTextView
+
+    init {
+        layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+        setBackgroundResource(R.drawable.ripple_rounded)
+        isClickable = true
+        isFocusable = true
+        orientation = VERTICAL
+        setPadding(15F.dp2Px().roundToInt(), 10F.dp2Px().roundToInt(), 15F.dp2Px().roundToInt(), 10F.dp2Px().roundToInt())
+
+        titleTextView = createTitleTextView()
+        addView(titleTextView)
+
+        subtitleTextView = createSubtitleTextView()
+        addView(subtitleTextView)
+    }
+
+    private object ViewId {
+        val TITLE_TEXT_VIEW = hashCode() + 1
+        val SUBTITLE_TEXT_VIEW = hashCode() + 2
+    }
+
+    private fun createTitleTextView(): MaterialTextView {
+        val textView = MaterialTextView(context)
+        textView.id = ViewId.TITLE_TEXT_VIEW
+        val layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+        textView.layoutParams = layoutParams
+        textView.letterSpacing = 0F
+        textView.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_dropdown_down, 0)
+        textView.compoundDrawablePadding = 10F.dp2Px().roundToInt()
+        textView.ellipsize = TextUtils.TruncateAt.END
+        textView.setSingleLine()
+        textView.maxLines = 1
+        textView.isAllCaps = false
+        textView.typeface = Typeface.create(Typeface.SANS_SERIF, Typeface.BOLD)
+        textView.setTextColor(Color.parseColor("#333333"))
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 17F)
+        return textView
+    }
+
+    private fun createSubtitleTextView(): MaterialTextView {
+        val textView = MaterialTextView(context)
+        textView.id = ViewId.SUBTITLE_TEXT_VIEW
+        val layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+        layoutParams.setMargins(0, 1F.dp2Px().roundToInt(), 0, 0)
+        textView.layoutParams = layoutParams
+        textView.letterSpacing = 0F
+        textView.ellipsize = TextUtils.TruncateAt.END
+        textView.setSingleLine()
+        textView.maxLines = 1
+        textView.includeFontPadding = false
+        textView.isAllCaps = false
+        textView.typeface = Typeface.create(Typeface.SANS_SERIF, Typeface.NORMAL)
+        textView.setTextColor(Color.parseColor("#8290A0"))
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 11F)
+        textView.text = "Текущий альбом"
+        return textView
+    }
+
+    fun setTitle(title: String) {
+        titleTextView.text = title
+    }
+
+    fun toggleIcon(isUp: Boolean) {
+        if (isUp) {
+            titleTextView.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_dropdown_up, 0)
+        } else {
+            titleTextView.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_dropdown_down, 0)
+        }
+    }
+
+}
